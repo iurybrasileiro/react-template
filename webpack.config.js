fbc749d50278ee76b90d38e9dc12c70093b9f5de
Iury Brasileiro <iury.brasileiro@outlook.com.br>
2021-09-13 22:38:10 -0300

    build: project initialized

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const { resolve } = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TerserWebpackPlugin = require('terser-webpack-plugin');
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+const config = {
+  mode: isProduction ? 'production' : 'development',
+  entry: {
+    index: resolve(__dirname, 'src', 'index.tsx'),
+  },
+  output: {
+    path: resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        loader: 'file-loader',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: resolve(__dirname, 'public', 'index.html'),
+      filename: 'index.html',
+      inject: 'root',
+    })
+  ],
+};
+
+if (isProduction) {
+  config.optimization = {
+    minimizer: [new TerserWebpackPlugin()],
+  };
+} else {
+  config.devServer = {
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+  };
+}
+
+module.exports = config;
